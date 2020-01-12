8d3b2dacd4ca8fa3b6eb89d7678ab58fdebf624f
Jeff Marek <jeffrey.marek@yahoo.com>
2020-01-12 16:41:56 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom';
+import {  Input } from "mdbreact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      results: [],
+	  search: ""
+    };
+  }
+
+  //Fetch is where we do the API call to retrieve the data from a public API
+  //https://github.com/toddmotto/public-apis 
+  componentDidMount() {
+    fetch("https://api.jikan.moe/v3/search/anime?q=onepunch&sort=ascending&order_by=title")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            results: result.results
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  onchange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+   const { error, isLoaded, results, search } = this.state;
+   const filteredResults = results.filter(item => {
+	   //alert(item.title.toLowerCase()); //Uncomment to troubleshoot outside of debugger
+      return item.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+  
+    //Alphabetize the filtered results
+    filteredResults.sort((a, b) => a.title.localeCompare(b.title));
+
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+  
+      //This is what will result as the JSX html output.
+      return (
+		  <div>
+        <img alt="One Punch" src={require('./imgs/OnePunchMan.jpg')} /> <br/>
+       <h1>Search through "One Punch" results</h1>  <br/>
+		  <Input label="label" icon="search" onChange={this.onchange} />
+        <ul>
+          {filteredResults.map(item => (
+            <li key={item.mal_id}>
+              {item.title} 
+            </li>
+          ))}
+        </ul>
+		</div>
+      );
+    }
+  }
 }
 
-export default App;
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {  Input } from "mdbreact";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      results: [],
+	  search: ""
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //Fetch is where we do the API call to retrieve the data from a public API
+  //https://github.com/toddmotto/public-apis 
+  componentDidMount() {
+    fetch("https://api.jikan.moe/v3/search/anime?q=onepunch&sort=ascending&order_by=title")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            results: result.results
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  onchange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+   const { error, isLoaded, results, search } = this.state;
+   const filteredResults = results.filter(item => {
+	   //alert(item.title.toLowerCase()); //Uncomment to troubleshoot outside of debugger
+      return item.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+  
+    //Alphabetize the filtered results
+    filteredResults.sort((a, b) => a.title.localeCompare(b.title));
+
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+  
+      //This is what will result as the JSX html output.
+      return (
+		  <div>
+        <img alt="One Punch" src={require('./imgs/OnePunchMan.jpg')} /> <br/>
+       <h1>Search through "One Punch" results</h1>  <br/>
+		  <Input icon="search" onChange={this.onchange} />
+        <ul>
+          {filteredResults.map(item => (
+            <li key={item.mal_id}>
+              {item.title} 
+            </li>
+          ))}
+        </ul>
+		</div>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
